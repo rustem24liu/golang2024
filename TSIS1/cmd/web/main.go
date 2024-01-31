e2c0c647bf98d9c550b9cc1a4fe8ef8f7ff6a192
rustem24liu <temirgalirustem05@gmail.com>
2024-01-31 20:51:24 +0600

    ups

--- TSIS1/cmd/web/main.go
+++ TSIS1/cmd/web/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/rustem24liu/golang2024/TSIS1/internal"
+	"github.com/rustem24liu/golang2024/TSIS1/internal/handlers"
 	"log"
 )
 
@@ -11,6 +11,6 @@ func main() {
 	log.Println("Starting API server")
 	r := mux.NewRouter()
 
-	r.HandleFunc("/home/bands", internal.getBands).Methods("GET")
+	r.HandleFunc("/home/bands", handlers.).Methods("GET")
 
 }
